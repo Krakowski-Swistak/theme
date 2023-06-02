17448026f3e84eeb561e4e06204e2ee603cbb3f5
Tomek <TK-54-4B>
2023-06-02 15:42:46 +0200

    tailwind cdn

--- .history/header_20230602154118.php
+++ .history/header_20230602154118.php
@@ -28,8 +28,6 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<link rel="profile" href="https://gmpg.org/xfn/11">
 
-	<script src="https://cdn.tailwindcss.com"></script>
-
 	
 	<?php wp_head(); ?>
 	<script>
