32e6664a87a8a637060e3165d34bd3ba3b68c753
Tomek <TK-54-4B>
2023-06-02 20:04:57 +0200

    career offer button

--- .history/front-page_20230602200350.php
+++ .history/front-page_20230602200350.php
@@ -362,7 +362,7 @@ get_header();
 												<?php echo $description; ?>
 											</p>
 											<button class="ks-button ks-button--primary">
-												 <a href="#"><?php echo $button; ?></a>
+												 <a href="#"><?php print( $button); ?></a>
 
 											</button>
 										</div>
