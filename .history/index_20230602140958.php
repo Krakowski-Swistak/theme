acf511a1afb4a045e22c84a0c2f4f91d7d6d49d7
Tomek <TK-54-4B>
2023-06-05 11:41:11 +0200

    blog page created

--- .history/index_20230602140958.php
+++ .history/index_20230602140958.php
@@ -1,8 +1,20 @@
 <?php
+/**
+ * The main template file
+ *
+ * This is the most generic template file in a WordPress theme
+ * and one of the two required files for a theme (the other being style.css).
+ * It is used to display a page when nothing more specific matches a query.
+ * E.g., it puts together the home page when no home.php file exists.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
+ *
+ * @package Swistak_Theme
+ */
 
 get_header();
-
 ?>
+
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main">
 			<div class="ks-container">
@@ -21,6 +33,11 @@ get_header();
 					while ( have_posts() ) :
 						the_post();
 
+						/*
+						* Include the Post-Type-specific template for the content.
+						* If you want to override this in a child theme, then include a file
+						* called content-___.php (where ___ is the Post Type name) and that will be used instead.
+						*/
 						get_template_part( 'template-parts/content', get_post_type() );
 
 					endwhile;
@@ -38,5 +55,4 @@ get_header();
 	</div><!-- #primary -->
 
 <?php
-
 get_footer();
