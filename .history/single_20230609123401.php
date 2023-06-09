2fcd878698652590494540145864b1f14f935092
Tomek <TK-54-4B>
2023-06-09 12:34:14 +0200

    related posts fix

--- .history/single_20230609123401.php
+++ .history/single_20230609123401.php
@@ -141,7 +141,7 @@ get_header();
 										'posts_per_page' => -1
 									);
 
-									$query = new WP_Query( array( 'category__in' => array( 7 ) ) );
+									$query = new WP_Query( array( 'category__in' => array( $7 ) ) );
 									if ($query->have_posts()) {
 										while ($query->have_posts()) {
 											$query->the_post();
