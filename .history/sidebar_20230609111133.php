9480d116022a9ce651dc37983f07d3dfd870e426
Tomek <TK-54-4B>
2023-06-09 11:12:34 +0200

    post page editing

--- .history/sidebar_20230609111133.php
+++ .history/sidebar_20230609111133.php
@@ -10,19 +10,6 @@
 if ( ! is_active_sidebar( 'sidebar-1' ) ) {
 	return;
 }
-
-$footer_phone = get_field('footer_phone', 'option');
-if( $footer_phone ): 
-    $footer_phone_url = $footer_phone['url'];
-    $footer_phone_title = $footer_phone['title'];
-    $footer_phone_target = $footer_phone['target'] ? $footer_phone['target'] : '_self';
-endif;
-$footer_email = get_field('footer_e-mail', 'option');
-if( $footer_email ): 
-    $footer_email_url = $footer_email['url'];
-    $footer_email_title = $footer_email['title'];
-    $footer_email_target = $footer_email['target'] ? $footer_email['target'] : '_self';
-endif;
 ?>
 
 <aside id="secondary" class="widget-area">
