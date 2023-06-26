da8c7d4d47ce655998590d3c82b78fcf1e8ad3c3
Tomek <TK-54-4B>
2023-06-26 12:17:31 +0200

    blog section post image

--- front-page.php
+++ front-page.php
@@ -392,7 +392,8 @@ get_header();
 								$postImageUrl = wp_get_attachment_image_src(get_post_thumbnail_id(), 'post');
 						?>
 							<div class="post-wrapper relative mb-[30px] desktop:w-1/3 w-fit">
-								<div class="post-img desktop:h-[300px] h-[200px] rounded-[10px] mb-[20px] overflow-hidden bg-[url('<?php echo $postImageUrl[0]; ?>')] bg-center bg-cover bg-no-repeat">
+								<div class="post-img group desktop:h-[300px] h-[200px] rounded-[10px] mb-[20px] overflow-hidden bg1-[url('<?php echo $postImageUrl[0]; ?>')] bg-center bg-cover bg-no-repeat ">
+									<img src="<?php echo $postImageUrl[0]; ?>" alt="post-image" class="scale-[1] group-hover:scale-[1.06] transition ease-out duration-[700ms]">
 								</div>
 								<a href="<?php the_permalink(); ?>" class="text-black hover:text-[#00b3a7] transition ease-out duration-200">
 									<?php the_title('<h3 class="desktop:text-[24px] text-[22px] font-semibold mb-0 desktop:leading-[30px] leading-[26px]">', '</h3>'); ?></php>
