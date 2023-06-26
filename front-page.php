268c5816823689e2b4395005cfbfcc6b8869eec1
Tomek <TK-54-4B>
2023-06-26 13:02:19 +0200

    bg image removed

--- front-page.php
+++ front-page.php
@@ -393,7 +393,7 @@ get_header();
 						?>
 							<div class="post-wrapper relative mb-[30px] desktop:w-1/3 w-fit">
 								<a href="<?php the_permalink(); ?>" class="text-black  hover:text-[#00b3a7] transition ease-out duration-200">
-									<div class="post-img group desktop:h-[300px] h-[200px] rounded-[10px] mb-[20px] overflow-hidden bg1-[url('<?php echo $postImageUrl[0]; ?>')] bg-center bg-cover bg-no-repeat ">
+									<div class="post-img group desktop:h-[300px] h-[200px] rounded-[10px] mb-[20px] overflow-hidden">
 										<img src="<?php echo $postImageUrl[0]; ?>" alt="post-image" class="object-cover scale-1 group-hover:scale-[1.06] transition ease-out duration-[700ms]">
 									</div>
 				
