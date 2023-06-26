756efc293ac3e6b9816c54b949e964e9e316b544
Tomek <TK-54-4B>
2023-06-26 15:01:54 +0200

    posts list edit

--- index.php
+++ index.php
@@ -51,7 +51,7 @@
 													<?php }; ?>
 												</ul>
 											</div>
-											<p class="mb-0 text-[15px] leading-[22px]"><?php echo wp_trim_words(get_the_excerpt(), 10); ?></p>
+											<p class="mb-0 text-[15px] leading-[22px]"><?php echo wp_trim_words(get_the_excerpt(), 16); ?></p>
 											<a href="<?php the_permalink(); ?>" class="text-black hover:text-[#00b3a7] transition ease-out duration-200 absolute bottom-[10px] right-[50px]">Czytaj więcej <span class="text-[20px]">&rarr;</span></a>
 										</div>
 									</div>
