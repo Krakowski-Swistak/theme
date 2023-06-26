9f12d879087d63c08e0fea3940547a9030a5d2dc
Tomek <TK-54-4B>
2023-06-26 18:09:36 +0200

    clients logos swiper

--- assets/public/dist/js/SwiperCarousels.js
+++ assets/public/dist/js/SwiperCarousels.js
@@ -95,6 +95,7 @@
       new Swiper("[data-slider-clients]", {
         slidesPerView: "auto",
         spaceBetween: 30,
+        loop: true,
         autoplay: {
           delay: 2000,
           disableOnInteraction: false,
