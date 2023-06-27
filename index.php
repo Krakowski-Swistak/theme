4cfd4ee9665b86422ed5b70859ac4f9d0992a4a8
Tomek <TK-54-4B>
2023-06-27 15:34:55 +0200

    blogpage edit

--- index.php
+++ index.php
@@ -33,12 +33,12 @@
 												<?php
 											}; ?>
 										<div class="post-text-wrapper relative basis-2/3">
-											<div class="post-date leading-[36px] desktop:flex hidden">
-												<img src="https://swistak.webo.design/wp-content/uploads/2023/06/calendar2.png" alt="calendar-icon" width="35" class="inline">
+											<div class="post-date text-[20px] leading-[36px] desktop:flex hidden">
+												<img src="https://swistak.webo.design/wp-content/uploads/2023/06/calendar2.png" alt="calendar-icon" width="20" class="inline">
 												<div class="date-text-wrapper ml-[6px] leading-0 desktop:block hidden">
 													<!--  -->
-													<span class="day mb-0 text-[36px] font-medium"><?php the_time('d'); ?></span>  
-													<span class="month mb-0 mt-[-30px] text-[20px] font-light"><?php the_time('M'); ?></span>
+													<span class="day mb-0 font-medium"><?php the_time('d'); ?></span>  
+													<span class="month mb-0 mt-[-30px] font-light"><?php the_time('M'); ?></span>
 												</div>
 											</div>
 											<h3 class="text-[25px] font-medium leading-[30px] mb-[10px]">
@@ -58,7 +58,7 @@
 												</ul>
 											</div>
 											<p class="mb-[40px] text-[15px] leading-[22px]"><?php echo wp_trim_words(get_the_excerpt(), 16); ?></p>
-											<a href="<?php the_permalink(); ?>" class="text-black hover:text-[#00b3a7] text-[16px] font-semibold transition ease-out duration-200 absolute bottom-[0px] desktop:left-auto desktop:right-[50px] left-[20px]">Czytaj więcej <span class="text-[20px]">&rarr;</span></a>
+											<a href="<?php the_permalink(); ?>" class="text-black hover:text-[#00b3a7] text-[16px] font-semibold transition ease-out duration-200 absolute bottom-[0px] desktop:left-auto desktop:right-[50px] left-0">Czytaj więcej <span class="text-[20px]">&rarr;</span></a>
 										</div>
 									</div>
 								<?php	
