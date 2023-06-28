2cfa45b1c14bc3319d6f2c54cc4fbabcabd3d055
Tomek <TK-54-4B>
2023-06-28 16:39:51 +0200

    test

--- functions.php
+++ functions.php
@@ -211,7 +211,7 @@ if( function_exists('acf_add_options_page') ) {
 }
 
 function add_menu_items_classname( $atts, $item, $args ) {
-	if(is_front_page() || is_home()){
+	if(is_front_page()){
 		$atts['class'] = 'scroll';
     return $atts;
 	}else return;
