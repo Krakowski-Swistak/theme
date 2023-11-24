73ad464d54e55793829b106f934368ca76204af5
Kamil <k.medrala99@gmail.com>
2023-11-24 11:02:36 +0100

    course duration styles, icon

--- tutor/single/course/course-entry-box.php
+++ tutor/single/course/course-entry-box.php
@@ -38,7 +38,7 @@ $default_meta            = array(
 	array(
 		'icon_class' => 'tutor-icon-clock-line',
 		'label'      => __( 'Duration', 'tutor' ),
-		'value'      => get_tutor_option( 'enable_course_duration' ) ? ( get_tutor_course_duration_context() ? '<div class="font-bold uppercase">' . __( 'Duration', 'tutor' ) . '</div>' . get_tutor_course_duration_context()  : false ) : null,
+		'value'      => get_tutor_option( 'enable_course_duration' ) ? ( get_tutor_course_duration_context() ? '<div class="font-bold uppercase ml-2 mr-auto">' . __( 'Duration', 'tutor' ) . '</div>' . get_tutor_course_duration_context()  : false ) : null,
 	),
 	array(
 		'icon_class' => 'tutor-icon-refresh-o',
@@ -84,26 +84,14 @@ $login_url    = tutor_utils()->get_option( 'enable_tutor_native_login', null, tr
 					</span>
 				</li>
 			<?php endif; ?>
-			<?php foreach ( $sidebar_meta as $key => $meta ) : ?>
-				<?php
-				if ( ! $meta['value'] || ( $meta['icon_class'] == "tutor-icon-level" )   ) {
-					continue;
-				}
-				?>
-				<li class="py-6 first:border-y border-b border-solid border-[#DDE3EE] flex items-center justify-between">
-					<span class="<?php echo esc_attr( $meta['icon_class'] ); ?> font-bold uppercase" aria-labelledby="<?php echo esc_html( $meta['label'] ); ?>"></span>
-					<span class="font-normal">
-						<?php echo wp_kses_post( $meta['value'] ); ?>
-					</span>
-				</li>
-			<?php endforeach; ?>
+
 			<?php if ( ! empty( $course_categories ) && is_array( $course_categories ) && count( $course_categories ) ) : ?>
-				<li class="py-6 border-b border-solid border-[#DDE3EE] flex items-center justify-between [&_a]:font-normal [&_a]:!text-[#132787]">
+				<li class="py-6 border-b border-solid border-[#DDE3EE] flex items-center justify-between [&_a]:font-normal [&_a]:!text-[#22272F]">
 					<span class="flex gap-2 items-center font-bold uppercase" aria-labelledby="Categories">
 						<svg xmlns="http://www.w3.org/2000/svg" width="16" height="20" viewBox="0 0 16 20" fill="none">
 							<path d="M8 20C3.664 20 0 17.965 0 15.556V4.444C0 2.035 3.664 0 8 0C12.336 0 16 2.035 16 4.444V15.556C16 17.965 12.337 20 8 20ZM2 12.9V15.559C2.07 16.112 4.309 17.781 8 17.781C11.691 17.781 13.931 16.107 14 15.553V12.9C12.1794 13.9554 10.1039 14.4905 8 14.447C5.89606 14.4906 3.82058 13.9554 2 12.9ZM2 7.341V10C2.07 10.553 4.309 12.222 8 12.222C11.691 12.222 13.931 10.548 14 9.994V7.341C12.1795 8.39678 10.104 8.93226 8 8.889C5.89596 8.93231 3.82046 8.39683 2 7.341ZM8 2.222C4.308 2.222 2.069 3.896 2 4.451C2.07 5 4.311 6.666 8 6.666C11.689 6.666 13.931 4.992 14 4.438C13.93 3.887 11.689 2.222 8 2.222Z" fill="#22272F"/>
 						</svg>
-						<?php esc_html_e( 'Categories:', 'tutor' ); ?>
+						<?php esc_html_e( 'Kategoria', 'tutor' ); ?>
 					</span>
 					<?php
 						$category_links = array();
@@ -117,9 +105,28 @@ $login_url    = tutor_utils()->get_option( 'enable_tutor_native_login', null, tr
 							array( 'a' => array( 'href' => true ) )
 						);
 					?>
-
 				</li>
 			<?php endif; ?>
+
+			<?php foreach ( $sidebar_meta as $key => $meta ) : ?>
+				<?php
+				if ( ! $meta['value'] || ( $meta['icon_class'] == "tutor-icon-level" )   ) {
+					continue;
+				}
+				?>
+				<li class="py-6 first:border-y border-b border-solid border-[#DDE3EE] flex items-center">
+					<?php if ( $meta['icon_class'] == "tutor-icon-clock-line" ) : ?>
+						<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
+							<path d="M11.999 22C7.05569 21.9349 3.06417 17.9433 2.99902 13C3.06417 8.05669 7.05569 4.06517 11.999 4.00002C16.9423 4.06517 20.9339 8.05669 20.999 13C20.9339 17.9433 16.9423 21.9349 11.999 22ZM11.999 6.00002C8.15418 6.05063 5.04963 9.15518 4.99902 13C5.04963 16.8448 8.15418 19.9494 11.999 20C15.8438 19.9494 18.9484 16.8448 18.999 13C18.9484 9.15518 15.8438 6.05063 11.999 6.00002ZM16.999 14H10.999V8.00002H12.999V12H16.999V14ZM20.291 6.70802L17.282 3.70802L18.691 2.29102L21.701 5.29102L20.291 6.70702V6.70802ZM3.70602 6.70802L2.29102 5.29102L5.28202 2.29102L6.69702 3.70802L3.70802 6.70802H3.70602Z" fill="#22272F"/>
+						</svg>
+					<?php else : ?>
+						<span class="<?php echo esc_attr( $meta['icon_class'] ); ?> font-bold uppercase" aria-labelledby="<?php echo esc_html( $meta['label'] ); ?>"></span>
+					<?php endif; ?>
+					<span class="font-normal w-full flex justify-end [&_span]:font-normal [&_span]:text-[#22272F]">
+						<?php echo wp_kses_post( $meta['value'] ); ?>
+					</span>
+				</li>
+			<?php endforeach; ?>
 		</ul>
 	</div>
 
