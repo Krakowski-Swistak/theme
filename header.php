d58ca29f84d0db0b4550a813571d40bf5437dc2d
Kamil <k.medrala99@gmail.com>
2023-11-28 11:59:38 +0100

    Revert "manifest.json link"
    This reverts commit de9b357123edeac8b6544ef8e8256980478e873f.

--- header.php
+++ header.php
@@ -28,7 +28,6 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<link rel="profile" href="https://gmpg.org/xfn/11">
 	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@9/swiper-bundle.min.css"/>
-	<link rel="manifest" href="/manifest.json">
 	<script src="https://cdn.tailwindcss.com"></script>
   <script>
       tailwind.config = {
