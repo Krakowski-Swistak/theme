676dbd7c3d35be334c67ef125513661b73c0cd6a
Kamil <k.medrala99@gmail.com>
2023-11-28 16:06:12 +0100

    margin bottom

--- tutor/archive-course-init.php
+++ tutor/archive-course-init.php
@@ -125,7 +125,7 @@ if ( isset( $the_query ) ) {
 	$supported_filters_keys = array_keys( $supported_filters );
 	?>
 	
-<div class="tutor-wrap tutor-wrap-parent tutor-courses-wrap tutor-container course-archive-page" data-tutor_courses_meta="<?php echo esc_attr( json_encode( $GLOBALS['tutor_course_archive_arg'] ) ); ?>">
+<div class="tutor-wrap tutor-wrap-parent tutor-courses-wrap tutor-container course-archive-page mb-20" data-tutor_courses_meta="<?php echo esc_attr( json_encode( $GLOBALS['tutor_course_archive_arg'] ) ); ?>">
 	<?php if ( $has_course_filters && in_array( 'search', $supported_filters_keys ) ) : ?>
 		<div class="tutor-d-block tutor-d-lg-none tutor-mb-32">
 			<div class="tutor-d-flex tutor-align-center tutor-justify-between">
