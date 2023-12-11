d1bb9267a852a5739f44fd1061ba3361669ebe50
Kamil <k.medrala99@gmail.com>
2023-12-11 12:50:12 +0100

    coupon checkout revert

--- functions.php
+++ functions.php
@@ -266,6 +266,6 @@ add_filter('previous_post_link', 'posts_link_prev_class');
 remove_action('woocommerce_checkout_order_review', 'woocommerce_checkout_payment', 20 );
 add_action('woocommerce_checkout_payment_hook', 'woocommerce_checkout_payment', 10 ); 
 
-remove_action( 'woocommerce_before_checkout_form', 'woocommerce_checkout_coupon_form', 10 );
-add_action( 'woocommerce_review_order_after_cart_contents', 'woocommerce_checkout_coupon_form', 10);
+// remove_action( 'woocommerce_before_checkout_form', 'woocommerce_checkout_coupon_form', 10 );
+// add_action( 'woocommerce_review_order_after_cart_contents', 'woocommerce_checkout_coupon_form', 10);
 
